c8c5c79dc29d8553129d7d71dac368e45776331a
Ezeofor PraiseGod <abuchipraise@gmail.com>
2025-06-03 21:26:35 +0100

    fix app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,70 +19,29 @@ const Page = async () => {
 		<main>
 			<h1 className='text-2xl'>Popular Companions</h1>
 			<section className='home-section'>
-				{userId ? (
-					companions.map((companion) => {
-						const color = getSubjectColor(companion.subject);
-						const border_Color = `hover:border-[${color}]`;
-						const shadowColor = getSubjectShadowColor(companion.subject);
+				{companions.map((companion) => {
+					const color = getSubjectColor(companion.subject);
+					const border_Color = `hover:border-[${color}]`;
+					const shadowColor = getSubjectShadowColor(companion.subject);
 
-						return (
-							<CompanionCard
-								key={companion.id}
-								{...companion}
-								color={color}
-								borderColor={border_Color}
-								shadowColor={shadowColor}
-							/>
-						);
-					})
-				) : (
-					<>
+					return (
 						<CompanionCard
-							id='456'
-							name='Serial, the number wizard'
-							topic='Understanding Infinity in Maths'
-							subject='maths'
-							duration={24}
-							color={getSubjectColor('maths')}
-							shadowColor={getSubjectShadowColor('maths')}
+							key={companion.id}
+							{...companion}
+							color={color}
+							borderColor={border_Color}
+							shadowColor={shadowColor}
 						/>
-						<CompanionCard
-							id='789'
-							name='Voca, the vocabulary builder'
-							topic='learning English'
-							subject='language'
-							duration={14}
-							color={getSubjectColor('language')}
-							shadowColor={getSubjectShadowColor('language')}
-						/>
-						<CompanionCard
-							id='678'
-							name='Artemis, the scientist'
-							topic='Understanding atoms and matter'
-							subject='science'
-							duration={30}
-							color={getSubjectColor('science')}
-							shadowColor={getSubjectShadowColor('science')}
-						/>
-					</>
-				)}
+					);
+				})}
 			</section>
 
 			<section className='home-section'>
-				{userId ? (
-					<CompanionsList
-						title='Recently Completed Sessions'
-						companions={recentSessionCompanions}
-						classNames='w-2/3 max-lg:w-full'
-					/>
-				) : (
-					<CompanionsList
-						title='Recent Sessions'
-						companions={recentSessions}
-						classNames='w-2/3 max-lg:w-full'
-					/>
-				)}
-
+				<CompanionsList
+					title='Recently Completed Sessions'
+					companions={recentSessionCompanions}
+					classNames='w-2/3 max-lg:w-full'
+				/>
 				<CTA />
 			</section>
 		</main>
